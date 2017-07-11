8eb478044e5e89c136f49068448676620ff74f15
tianqi <627764670@qq.com>
2017-07-11 21:42:08 +0800

    tt2

--- js/mytools.js
+++ js/mytools.js
@@ -134,64 +134,7 @@
 			return window.getComputedStyle(obj,false)[attrName];
 		}
 	}
-	/*
-	功能：设置cookie
-	参数1：键名
-	参数2：值
-	参数3：过期值（天数）
-	返回值：true:false
-	**/	
-	function setCookie(key,value,time){
-		if(isCookie()){
-			var date=new Date();
-			date.setDate(date.getDate()+time);
-			document.cookie=key+"="+value+"; expires="+date.toGMTString();
-			return true;
-		}else{
-			return false;
-		}
-	}
-	/*
-	功能：获取cookie
-	参数1：键名
-	返回值：string:null:undefined
-	**/	
-	function getCookie(key){
-		if(isCookie()){
-			var arr=document.cookie.split("; ");
-			for(var i in arr){
-				var item=arr[i].split("=");
-				if(item[0]==key){
-					return item[1];
-				}
-			}
-			return null;
-		}else{
-			return undefined;
-		}
-	 }
-	/*
-	功能：判断浏览器是否禁用cookie
-	返回值：true:false
-	**/	
-	 function isCookie(){
-		return navigator.cookieEnabled?true:false;
-	 }
-	 /*
-	功能：删除cookie
-	参数1：键名
-	返回值：true:false
-	**/	
-	function delCookie(key){
-		if(isCookie()){
-			var date=new Date();
-			date.setDate(date.getDate()-1);
-			document.cookie=key+"=1; expires="+date.toGMTString();
-			return true;
-		}else{
-			return false;
-		}	
-	}
+
 	 /*
 	功能：正则验证
 	参数1：验证类型
@@ -238,3 +181,51 @@
 			break;
 		}  
 	} 
+
+
+
+//功能：保存cookie
+//参数：
+//key：键
+//value：值
+//dayCount：有效期（单位是天）
+//返回值：无
+function saveCookie(key,value,dayCount){
+	var d = new Date();
+	d.setDate(d.getDate()+dayCount);
+	document.cookie = key+"="+encodeURIComponent(value)+";expires="+d.toGMTString();	
+}
+
+
+//功能：读取cookie（根据键读取对应的值）
+//参数：
+//key：键
+//返回值：值，""：表示没有找到对应的cookie；
+
+//cssfile=red; aauserName=ttt; userName=jzm
+function getCookie(key){	
+	var str = decodeURIComponent(document.cookie);
+	//1、转换成数组
+	var arr = str.split("; ");
+	//2、根据键找到对应的数组元素
+	var index=-1;
+	for(var i=0;i<arr.length;i++){
+		if(arr[i].indexOf(key+"=")==0){
+			index = i;
+			break;
+		}
+	}
+	//3、截取出值
+	if(index==-1){
+		return "";
+	}else{
+		return arr[index].substring(key.length+1);
+	}
+}
+
+//功能：删除cookie(根据键删除cookie)
+//参数：
+//key：键；
+function removeCookie(key){
+	saveCookie(key,"",-1);
+}
